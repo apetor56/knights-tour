1e65f447c3c7be6eb8c48b14045d4e82ab137d34
apetor56 <lukten.github@gmail.com>
2022-07-31 23:18:54 +0200

    set position attrib as vec2 and color as float

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -1,11 +1,11 @@
 #version 330
 
-layout(location = 0) in vec3 iPosition;
-layout(location = 1) in vec3 iColor;
+layout(location = 0) in vec2 iPosition;
+layout(location = 1) in float iColor;
 
 out vec4 oColor;
 
 void main() {
-    gl_Position = vec4(iPosition.x, iPosition.y, iPosition.z, 1.0);
-    oColor = vec4(iColor.x, iColor.y, iColor.z, 1.0);
+    gl_Position = vec4(iPosition.x, iPosition.y, 0.0, 1.0);
+    oColor = vec4(iColor * 1, iColor * 1, iColor * 1, 1.0);
 }
