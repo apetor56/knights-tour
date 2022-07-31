1d739980af9b36e019636d808636df0621a4ec4d
apetor56 <lukten.github@gmail.com>
2022-07-31 23:19:53 +0200

    initial chessboard drawing

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,17 @@
 #include <iostream>
-#include <glad.h>
+#include <vector>
+#include "Renderer.hpp"
 #include "Window.hpp"
-#include "Shader.hpp"
-#include "VertexArray.hpp"
-#include "Buffer.hpp"
-#include "Data.hpp"
 
 int main(int argc, const char *argv[]) {
     try {
-        Window window("ja nie komar", 800, 800);
-
-        VertexArray<> vao;
-        
-        Buffer<GL_ARRAY_BUFFER, float> vbo;
-        vbo.setData(vertices, sizeof(vertices));
-
-        AttributeLayout<float> layout(POSITION | COLOR);
-        vbo.vertexAttrib(Position, layout);
-        vbo.vertexAttrib(Color, layout);
-
-        Buffer<GL_ELEMENT_ARRAY_BUFFER, unsigned int> ebo;
-        ebo.setData(indices, sizeof(indices));
-
-        Shader vertex(GL_VERTEX_SHADER, "../shaders/vertex.glsl");
-        Shader fragment(GL_FRAGMENT_SHADER, "../shaders/fragment.glsl");
-
-        unsigned int shaderProgram = Shader::createProgramShader(vertex.getShader(), fragment.getShader());
-        Shader::useProgram(shaderProgram);
-
+        Window window("ja nie komar", 600, 600);
+        Renderer renderer;
+    
+        if(argc != 2) {
+            return -1;
+        }
+        renderer.setChessBoard(std::stoi(argv[1]));
         // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
  
         while(!window.shouldClose()) {
@@ -35,8 +19,7 @@ int main(int argc, const char *argv[]) {
 
             window.processInput();
             
-            // glDrawArrays(GL_TRIANGLES, 0, 3);
-            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)0);
+            renderer.drawChessBoard();
 
             window.swapBuffers();
 
