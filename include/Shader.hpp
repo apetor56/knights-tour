e4d6e6367a3191b8b4f38ff92fc28e39ca7f9f79
apetor56 <lukten.github@gmail.com>
2022-07-31 23:12:09 +0200

    added vertex attrib functions from Buffer.hpp

--- include/Shader.hpp
+++ include/Shader.hpp
@@ -3,9 +3,12 @@
 
 #include <optional>
 #include "Exception.hpp"
+#include "AttributeLayout.hpp"
 
 class Shader {
 public:
+    Shader() = default;
+    
     explicit Shader(unsigned int type, const char *source);
 
     Shader(const Shader& other) = delete;
@@ -20,10 +23,40 @@ public:
 
     unsigned int getShader() const noexcept;
 
+    static void enableAttrib(int index);
+
+    static void disableAttrib(int index);
+
+    // execute glEnableVertexAttribArray() and glVertexAttribPointer() functions
+    // with given attrib and stride and offset given in attribute layout
+    template<typename T>
+    static void vertexAttrib(Attributes attrib, AttributeLayout<T>& layout) {
+        int stride = layout.getStride();
+        void *offset = layout.getOffset(attrib);
+        const int& attribSize = layout.getAttribSize(attrib);
+
+        glEnableVertexAttribArray(attrib);
+        glVertexAttribPointer(attrib, attribSize, dataType<T>(), GL_FALSE, stride, offset);
+    }
+
 private:
     unsigned int _shaderID;
 
     static std::optional<std::string> getShaderSourceCode(const char *name);
+
+    // returns specific GL macro depeding on type od data
+    // stored in buffers
+    template<typename T>
+    static int dataType() {
+        const std::type_info& type = typeid(T);
+        if(type == typeid(float)) {
+            return GL_FLOAT;
+        }
+        if(type == typeid(unsigned int)) {
+            return GL_UNSIGNED_INT;
+        }
+        return -1;
+    }
 };
 
 class ShaderException : public Exception {
