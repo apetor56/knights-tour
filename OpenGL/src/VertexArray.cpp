fec2d33a8bde71feb1586ab9590871e36ce5020a
apetor56 <lukten.github@gmail.com>
2022-08-11 21:41:28 +0200

    vertex array class implementation

---
+++ OpenGL/src/VertexArray.cpp
@@ -0,0 +1,26 @@
+#include <glad.h>
+#include <stdexcept>
+#include <iostream>
+
+#include "VertexArray.hpp"
+
+//-------------------------------------------------------------//
+//-------------- VertexArray class implementation -------------//
+//-------------------------------------------------------------//
+
+VertexArray::VertexArray() {
+    glGenVertexArrays(1, &_vertexArrayID);
+    glBindVertexArray(_vertexArrayID);
+}
+
+void VertexArray::bind() const {
+    glBindVertexArray(_vertexArrayID);
+}
+
+void VertexArray::unbind() const noexcept {
+    glBindVertexArray(0);
+}
+
+unsigned int VertexArray::getID() const {
+    return _vertexArrayID;
+}
