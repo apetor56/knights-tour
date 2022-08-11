0ab2c8faee28619093a0413e5eeceb35fe7d7678
apetor56 <lukten.github@gmail.com>
2022-08-11 21:39:54 +0200

    renders chessboard and knight movements

---
+++ OpenGL/src/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <thread>
+#include <chrono>
+#include "Renderer.hpp"
+#include "Texture2D.hpp"
+#include "Window.hpp"
+
+void test(Window& window) {
+    window.pollEvents();
+    window.processInput();
+}
+
+int main(int argc, const char *argv[]) {
+    try {
+        Window window("ja nie komar", 800, 800);
+        Renderer renderer;
+
+        if(argc != 3) {
+            return -1;
+        }
+
+        int size = atoi(argv[1]);
+        int startPos = atoi(argv[2]);
+
+        renderer.setChessBoard(size, startPos);
+        // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+        int x = 0, old_x = 0;
+        int y = 0, old_y = 0;
+        int i = 0;
+        while(!window.shouldClose()) {
+            window.clearBuffer();
+
+            window.processInput();
+            
+            renderer.drawChessBoard();
+
+            if(i < size * size) {
+                renderer.drawKnight(x, y);
+                std::cin >> x >> y;
+                i++;
+            }
+            else {
+                renderer.drawKnight(old_x, old_y);
+            }
+
+            window.swapBuffers();
+            window.pollEvents();
+
+            if(i < size * size) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+            }
+        }
+    }
+    catch(Exception& e) {
+        std::cout << e.what() << '\n';
+    }
+    catch(std::exception& e) {
+        std::cout << e.what() << '\n';
+    }
+    catch(...) {
+        std::cout << "undefined exception\n";
+    }
+}
