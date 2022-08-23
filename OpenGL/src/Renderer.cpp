17979ef2a5a46c57ebe9f7f03ae0625f5917f443
apetor56 <lukten.github@gmail.com>
2022-08-24 00:19:32 +0200

    implement drawing line function

--- OpenGL/src/Renderer.cpp
+++ OpenGL/src/Renderer.cpp
@@ -11,18 +11,27 @@ const char *vertexPath_chess = "../shaders/vertex.glsl";
 const char *fragmentPath_chess = "../shaders/fragment.glsl";
 const char *vertexPath_knight = "../shaders/vertex_knight.glsl";
 const char *fragmentPath_knight = "../shaders/fragment_knight.glsl";
+const char *vertexPath_line = "../shaders/vertex_line.glsl";
+const char *fragmentPath_line = "../shaders/fragment_line.glsl";
 const char *texturePath = "../tex/knight.png";
+const char *texturePath2 = "../tex/gachi.png";
 
-Renderer::Renderer() 
+Renderer::Renderer(int size) 
 :   _vertexChess(GL_VERTEX_SHADER, vertexPath_chess),
     _fragmentChess(GL_FRAGMENT_SHADER, fragmentPath_chess),
     _vertexKnight(GL_VERTEX_SHADER, vertexPath_knight),
-    _fragmentKnight(GL_FRAGMENT_SHADER, fragmentPath_knight)
-{}
+    _fragmentKnight(GL_FRAGMENT_SHADER, fragmentPath_knight),
+    _vertexLine(GL_VERTEX_SHADER, vertexPath_line),
+    _fragmentLine(GL_FRAGMENT_SHADER, fragmentPath_line),
+    _size(size)
+{
+    _programChess = Shader::createProgramShader(_vertexChess, _fragmentChess);
+    _programKnight = Shader::createProgramShader(_vertexKnight, _fragmentKnight);
+    _programLine = Shader::createProgramShader(_vertexLine, _fragmentLine);
+}
 
 // set VAO, VBO, EBO and shaders for generated data 
-void Renderer::setChessBoard(int size, int startPos) {
-    _size = size;
+void Renderer::setChessBoard(int startPos) {
     _startPos = startPos;
     generateData(_size);
     _vaoChess.bind();
@@ -30,25 +39,21 @@ void Renderer::setChessBoard(int size, int startPos) {
     _vboChess.bind();
     _vboChess.setData(_chessData, sizeof(float) * _size * _size * 12);
     _eboChess.bind();
-    _eboChess.setData(_indexes, _size * size * 6 * sizeof(unsigned int));
+    _eboChess.setData(_indexes, _size * _size * 6 * sizeof(unsigned int));
 
     AttributeLayout<float> layout(POSITION | COLOR);
     layout.setAttribSize(Position, 2);
     layout.setAttribSize(Color, 1);
 
-    // Shader::enableAttrib(Position);
     Shader::vertexAttrib(Position, layout);
-    // Shader::enableAttrib(Color);
     Shader::vertexAttrib(Color, layout);
-
-    unsigned int program = Shader::createProgramShader(_vertexChess.getShader(), _fragmentChess.getShader());
-    _programChess = program;
 }
 
 void Renderer::drawChessBoard() const {
     _vaoChess.bind();
     Shader::useProgram(_programChess);
     glDrawElements(GL_TRIANGLES, _size * _size * 6, GL_UNSIGNED_INT, (void*)0);
+    _vaoChess.unbind();
 }
 
 // calculates square's position depending on given coordinates
@@ -132,6 +137,7 @@ void Renderer::generateData(int n) {
 void Renderer::setKnightData(float start_x, float start_y, float length) {
     start_x = start_x + (_startPos % _size) * length;
     start_y = start_y - (_startPos / _size) * length;
+    initializeLine(start_x + length / 2, start_y - length / 2);
 
     float knightData[] {
         start_x, start_y - length, 0.0f, 0.0f,
@@ -156,7 +162,6 @@ void Renderer::setKnightData(float start_x, float start_y, float length) {
     Shader::vertexAttrib(Position, layout);
     Shader::vertexAttrib(Texture, layout);
 
-    _programKnight = Shader::createProgramShader(_vertexKnight.getShader(), _fragmentKnight.getShader());
     Shader::useProgram(_programKnight);
 
     Texture2D texture;
@@ -164,23 +169,32 @@ void Renderer::setKnightData(float start_x, float start_y, float length) {
     texture.bind();
     texture.setData(texturePath, GL_RGBA);
     texture.setUniformSampler(_programKnight, "knightTex", GL_TEXTURE0);
+
+    texture.activeTex(GL_TEXTURE1);
+    texture.setData(texturePath2, GL_RGB);
+    texture.setUniformSampler(_programKnight, "gachiTex", GL_TEXTURE1);
 }
 
 void Renderer::drawKnight(int move_x, int move_y) {
-    _vaoKnight.bind();
-    Shader::useProgram(_programKnight);
-
     static float length = 1.6f / _size;
     static float x = 0.0f;
     static float y = 0.0f;
     x += move_x * length;
     y += move_y * length;
 
+    updateLine(move_x * length, move_y * length);
+    _vaoLine.bind();
+    Shader::useProgram(_programLine);
+    glDrawElements(GL_LINES, (_lineCounter - 1) * 2, GL_UNSIGNED_INT, (void*)0);
+    
+    _vaoKnight.bind();
+    Shader::useProgram(_programKnight);
+
     glm::mat4 transform(1.0f);
     transform = glm::translate(transform, glm::vec3(x, y, 0.0f));
     unsigned int pos = glGetUniformLocation(_programKnight, "transformMatrix");
     glUniformMatrix4fv(pos, 1, GL_FALSE, glm::value_ptr(transform));
-    
+
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)0);
 }
 
@@ -207,5 +221,52 @@ void Renderer::setIndexes() {
         _indexes[squareVertexID + 3] = i + 2 + vertexID;
         _indexes[squareVertexID + 4] = i + 3 + vertexID;
         _indexes[squareVertexID + 5] = i + vertexID;
+    }
+}
+
+void Renderer::initializeLine(float x, float y) {
+    glLineWidth(3.0f / _size * 8.0f);
+    _verticesLine = new float[2 * _size * _size]();
+    _indexesLine = new unsigned int[2 * _size * _size - 2]();
+
+    _verticesLine[0] = x;
+    _verticesLine[1] = y;
+    setLineIndexes();
+
+    _vaoLine.bind();
+    _vboLine.bind();
+    _eboLine.bind();
+    _vboLine.setData(_verticesLine, sizeof(float) * 2 * _size * _size);
+    _eboLine.setData(_indexesLine, sizeof(unsigned int) * 2 * (_size * _size - 1));
+
+    AttributeLayout<float> layout(POSITION);
+    layout.setAttribSize(Position, 2);
+    Shader::vertexAttrib(Position, layout);
+    _vaoLine.unbind();
+    _lineCounter = 1;
+}
+
+void Renderer::updateLine(float x, float y) {
+    if(!(_lineCounter < _size * _size) || (x == 0.0f && y == 0.0f)) {
+        return;
+    }
+
+    _vboLine.bind();
+    _eboLine.bind();
+    _verticesLine[2 * _lineCounter] = _verticesLine[2 * (_lineCounter - 1)] + x;
+    _verticesLine[2 * _lineCounter + 1] = _verticesLine[2 * (_lineCounter - 1) + 1] + y;
+    
+    glBufferSubData(GL_ARRAY_BUFFER, sizeof(float) * 2 * _lineCounter, sizeof(float), &_verticesLine[2 * _lineCounter]);
+    glBufferSubData(GL_ARRAY_BUFFER, sizeof(float) * (2 * _lineCounter + 1), sizeof(float), &_verticesLine[2 * _lineCounter + 1]);
+
+    _lineCounter++;
+}
+
+void Renderer::setLineIndexes() {
+    _indexesLine[0] = 0u;
+    _indexesLine[1] = 1u;
+
+    for(unsigned int i = 2u; i < 2 * (_size * _size - 1u); i++) {
+        _indexesLine[i] = _indexesLine[i - 1] + i % 2;
     }
 }
