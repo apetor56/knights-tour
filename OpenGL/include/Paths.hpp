bd7e5a2c5572caba622c7228e0b7d4ad53e040e1
apetor56 <lukten.github@gmail.com>
2022-08-24 19:03:32 +0200

    extracts paths from Renderer.cpp to seperate header file

---
+++ OpenGL/include/Paths.hpp
@@ -0,0 +1,15 @@
+#ifndef PATHS_HPP
+#define PATHS_HPP
+
+/* contains all paths needed to create shaders and textures */
+
+const char *vertexPath_chess = "../shaders/vertex.glsl";
+const char *fragmentPath_chess = "../shaders/fragment.glsl";
+const char *vertexPath_knight = "../shaders/vertex_knight.glsl";
+const char *fragmentPath_knight = "../shaders/fragment_knight.glsl";
+const char *vertexPath_line = "../shaders/vertex_line.glsl";
+const char *fragmentPath_line = "../shaders/fragment_line.glsl";
+const char *texturePath_knight = "../tex/knight.png";
+const char *texturePath_gachi = "../tex/gachi.png";
+
+#endif // PATHS_HPP
