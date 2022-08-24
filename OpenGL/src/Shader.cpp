a7ea58e08fe3e63a9be2fce4c8e1c9da9e6f7d2f
apetor56 <lukten.github@gmail.com>
2022-08-24 19:08:15 +0200

    deleted std::optional and added some comments

--- OpenGL/src/Shader.cpp
+++ OpenGL/src/Shader.cpp
@@ -15,15 +15,17 @@
 Shader::Shader(unsigned int type, const char *sourceFile) {
     _shaderID = glCreateShader(type);
 
-    std::string s_src = *Shader::getShaderSourceCode(sourceFile);
+    /* loads shader from source file to string and then to const char* */
+    std::string s_src = Shader::getShaderSourceCode(sourceFile);
     const char *src = s_src.c_str();
     
+    /* loads and compiles shader */
     glShaderSource(_shaderID, 1, &src, nullptr);
     glCompileShader(_shaderID);
 
+    /* checks if shader was compilled successfully */
     char infoLog[512];
     int success;
-
     glGetShaderiv(_shaderID, GL_COMPILE_STATUS, &success);
     if(!success) {
         glGetShaderInfoLog(_shaderID, 512, nullptr, infoLog);
@@ -40,20 +42,21 @@ Shader::operator unsigned int() {
 }
 
 // get shader from source file with given path
-std::optional<std::string> Shader::getShaderSourceCode(const char *name) {
+std::string Shader::getShaderSourceCode(const char *name) {
     std::ostringstream oss;
     std::fstream file(name);
 
     if(!file) {
-        std::cout << name << ": failed to open shader source file\n";
-        return {};
+        throw std::runtime_error("failed to open shader source file");
     }
 
+    /* saves source file to ostringstream object */
     std::string line;
     while(std::getline(file, line)) {
         oss << line << '\n';
     }
     
+    /* returns oss content as string */
     return oss.str();
 }
 
@@ -61,14 +64,14 @@ std::optional<std::string> Shader::getShaderSourceCode(const char *name) {
 unsigned int Shader::createProgramShader(unsigned int s1, unsigned int s2) {
     unsigned int program = glCreateProgram();
 
+    /* attaches shaders and links program */
     glAttachShader(program, s1);
     glAttachShader(program, s2);
-
     glLinkProgram(program);
 
+    /* checks if program was linked successfully */
     char infoLog[512];
     int success;
-
     glGetProgramiv(program, GL_LINK_STATUS, &success);
     if(!success) {
         glGetProgramInfoLog(program, 512, nullptr, infoLog);
@@ -87,7 +90,7 @@ unsigned int Shader::getShader() const noexcept {
 }
 
 void Shader::enableAttrib(int index) {
-        glEnableVertexAttribArray(index);
+    glEnableVertexAttribArray(index);
 }
 
 void Shader::disableAttrib(int index) {
