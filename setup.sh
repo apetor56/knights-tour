509063dfedbc105e4df8b04921ad0ca14fe117d1
Łukasz Tenerowicz <99681079+apetor56@users.noreply.github.com>
2022-12-26 14:19:38 +0100

    Delete setup.sh

--- setup.sh
+++
@@ -1,7 +0,0 @@
-cd lib
-cmake -S . -B ./build -G "Unix Makefiles" -D CMAKE_BUILD_TYPE=Release
-cmake --build ./build
-
-cd ../
-cmake -S . -B ./build -G "Unix Makefiles" -D CMAKE_BUILD_TYPE=Release
-cmake --build ./build
